b5ebafec23916c54a518286e3490b5b4d0352bb9
rmsgnl11 <rmsgnl11@naver.com>
2025-09-24 21:09:24 +0900

    feat(execution): MatchingEngine 도메인 테스트 코드 작성 및 test - h2 인메모리 사용

--- services/execution-service/src/main/java/com/jjikmuk/execution_service/infrastructure/persistence/repository/OutboxJpaRepository.java
+++ services/execution-service/src/main/java/com/jjikmuk/execution_service/infrastructure/persistence/repository/OutboxJpaRepository.java
@@ -1,4 +1,9 @@
 package com.jjikmuk.execution_service.infrastructure.persistence.repository;
 
-public class OutboxJpaRepository {
+import com.jjikmuk.execution_service.infrastructure.persistence.entity.OutboxEventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OutboxJpaRepository extends JpaRepository<OutboxEventEntity, UUID> {
 }
