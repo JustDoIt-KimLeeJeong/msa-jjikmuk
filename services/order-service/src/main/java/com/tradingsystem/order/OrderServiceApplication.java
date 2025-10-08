68bf7795a6d5e51b9b651b90c5fe2274574b6c87
yunha <dbsgk45@naver.com>
2025-10-08 19:33:36 +0900

    feat(order): order service codebase 변경 - execution service와 java 버전, 의존성 통일

--- services/order-service/src/main/java/com/tradingsystem/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/tradingsystem/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.jjikmuk.order;
+package com.tradingsystem.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
